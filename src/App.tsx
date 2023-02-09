a99be41c90630acddb242e9d1660c6500b3121c5
Rafael Costa Palma <rafael.palma@opah.com.br>
2023-02-09 11:07:53 -0300

    refactoring context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Home } from './components/Home'
 import { About } from './components/About'
-import { useState } from 'react'
-import { MenuContext } from './context/MenuContext'
+import { MenuContextProvider } from './context/MenuContext'
 
 function App() {
 
-  const [name, setName ] = useState('Fernanda') 
-
   return (
     <div className="App">
-      <MenuContext.Provider value={{ name, setName }}>
+      <MenuContextProvider>
         <About />
         ******
         <Home />
-      </MenuContext.Provider>
+      </MenuContextProvider>
     </div>
   )
 }
--- src/context/MenuContext/index.tsx
+++ src/context/MenuContext/index.tsx
@@ -1,3 +1,22 @@
-import { createContext } from 'react'
+import { createContext, ReactNode, useState } from 'react'
+
+interface MenuContextProviderProps {
+  children: ReactNode
+}
 
 export const MenuContext = createContext({} as any)
+
+export const MenuContextProvider = ({ children }: MenuContextProviderProps) => {
+  const [name, setName ] = useState('Fernanda') 
+
+  return (
+    <MenuContext.Provider
+      value={{
+        name,
+        setName
+      }}
+    >
+      { children }
+    </MenuContext.Provider>
+  )
+}
